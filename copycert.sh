4e652c7818a054618cf654a3221c5d4f5f507a70
elfelip <elfelip@yahoo.com>
2020-12-07 15:00:53 -0500

    Copie des certificats sur hosts kubernetes et registre pour Keycloak

--- copycert.sh
+++ copycert.sh
@@ -1,11 +1,13 @@
+CERTFILE=${1:-resources/cert/lacave-root-ca.crt}
+PEMFILE=${2:-resources/cert/lacave-root.pem}
 HOSTS="
 kube01
 kube02
 kube03"
 
 for HOST in $HOSTS; do
-  scp resources/cert/lacave-root.pem root@${HOST}:/etc/pki/ca-trust/source/anchors
+  scp $CERTFILE root@${HOST}:/etc/pki/ca-trust/source/anchors
   ssh root@${HOST} update-ca-trust
   ssh root@${HOST} mkdir -p /etc/kubernetes/ssl
-  scp resources/cert/lacave-root.pem root@${HOST}:/etc/kubernetes/ssl
+  scp $PEMFILE root@${HOST}:/etc/kubernetes/ssl
 done
